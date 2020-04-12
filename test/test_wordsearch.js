13630f5094b68b1c0eaf53c3a1153d871f9ef348
Judith Huynh <judithuynh@gmail.com>
2020-04-12 20:55:04 +0000

    add tests that word can be found vertically and makes sure that all pass tests

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -4,6 +4,8 @@ const assert = chai.assert;
 const wordSearch = require('../wordsearch.js')
 
 describe("#wordSearch()", function() {
+  // empty array; 
+  
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -35,4 +37,25 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
-});
+  it("should return undefined if there is no letters", function() {
+    const result = wordSearch([], "john");
+    assert.isUndefined(result)
+  });
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+    ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+    ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+    ['W', 'H', 'O', 'S', 'Y', 'E', 'R', 'L'],
+    ['B', 'F', 'H', 'E', 'N', 'E', 'Y', 'B'],
+    ['U', 'B', 'N', 'W', 'A', 'P', 'A', 'I'],
+    ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+    ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+  ], 'JOHN')
+  assert.isTrue(result);
+  });
+ });
+
+
+// lf, "Do the current tests cover all the possible cases?" What if the word is written vertically, not horizontally? You'll have to write tests for these cases (and any others that you think of), and you might also have to modify the wordSearch function to make those new tests pass.
