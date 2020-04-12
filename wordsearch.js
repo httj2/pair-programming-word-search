2afcb114c6dcedda2e4607416724fae305dccd21
Judith Huynh <judithuynh@gmail.com>
2020-04-12 20:53:49 +0000

    made new changes that accepts vertical seraching fro the word and empty word arrays

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,22 @@
 const wordSearch = (letters, word) => { 
+    if (letters.length > 0 ) {
+    // horizontal searching
     const horizontalJoin = letters.map(ls => ls.join(''))
+    // vertical searching   
+    const verticalJoin = letters[0].map((x, i) => letters.map(x => x[i]).join(''))
+    // console.log(verticalJoin);
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) 
+        return true
     }
+    for (l of verticalJoin) {
+        if (l.includes(word)) 
+        return true
+    }
+    return false
+    } 
+    return undefined
 }
 
-module.exports = wordSearch
+module.exports = wordSearch;
+// console.log(wordSearch([], "john"));
